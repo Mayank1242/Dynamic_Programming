accf840d1443fd84150eedc110c385c00a4698fc
Mayank mishra <90859312+Mayank1242@users.noreply.github.com>
2022-06-14 14:22:23 -0700

    commit

--- Dynamic_Programing/src/concepts/P02_Factorial_Tabulation_Method.java
+++ Dynamic_Programing/src/concepts/P02_Factorial_Tabulation_Method.java
@@ -3,12 +3,12 @@ package concepts;
 public class P02_Factorial_Tabulation_Method {
 
 	public static int fib(int n) {
-		if(n<=1 ) {
-			return n;
-		}
-		
-		
-		 return fib(n-1)+fib(n-2);
+		int f[] = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i <= n; i++)
+            f[i] = f[i - 1] + f[i - 2];
+        return f[n];
 	
 	}
 public static void main(String args[]) {
